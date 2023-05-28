6ce40f8bbcf8bd495a0eff6ea905c5c1b706487f
officialrashid <muhammedrashi59@gmail.com>
2023-05-28 13:49:25 +0530

    code format done

--- src/Components/movieDetails/movieDetails.jsx
+++ src/Components/movieDetails/movieDetails.jsx
@@ -1,43 +1,44 @@
-import React, { useEffect, useState } from 'react';
-import axios from '../../axios';
-import { imageUrl, API_KEY } from '../../constant/constant';
-import './movieDetails.css'
-import { useParams } from 'react-router-dom';
-import Youtube from 'react-youtube'
-
+import React, { useEffect, useState } from "react";
+import axios from "../../axios";
+import { imageUrl, API_KEY } from "../../constant/constant";
+import "./movieDetails.css";
+import { useParams } from "react-router-dom";
+import Youtube from "react-youtube";
 
 function MovieDetails() {
   const [movie, setMovie] = useState();
   const { id } = useParams();
-  const [urlid,seturlid]=useState('')
+  const [urlid, seturlid] = useState("");
 
   useEffect(() => {
-    axios.get(`/movie/${id}?api_key=${API_KEY}&language=en-US`).then((response) => {
-      setMovie(response.data);
-    });
+    axios
+      .get(`/movie/${id}?api_key=${API_KEY}&language=en-US`)
+      .then((response) => {
+        setMovie(response.data);
+      });
   }, [id]);
 
   const opts = {
-    height: '390',
-    width: '100%',
+    height: "390",
+    width: "100%",
     playerVars: {
       // https://developers.google.com/youtube/player_parameters
       autoplay: 1,
     },
   };
-  const movievideo=(id)=>{
-    console.log(id,"sfdhjdsfhjdshjdshjdsjdsjdjsfj");
-    axios.get(`/movie/${id}/videos?api_key=${API_KEY}&language=en-US`).then(response=>{
-      console.log(response.data);
-      if(response.data.results.length!==0){
-        seturlid(response.data.results[0])
-      }else{
-        console.log("empty ");
-      }
-
-    })
-
-  }
+  const movievideo = (id) => {
+    console.log(id, "sfdhjdsfhjdshjdshjdsjdsjdjsfj");
+    axios
+      .get(`/movie/${id}/videos?api_key=${API_KEY}&language=en-US`)
+      .then((response) => {
+        console.log(response.data);
+        if (response.data.results.length !== 0) {
+          seturlid(response.data.results[0]);
+        } else {
+          console.log("empty ");
+        }
+      });
+  };
 
   if (!movie) {
     return <div>Loading...</div>;
@@ -46,36 +47,34 @@ function MovieDetails() {
   return (
     <div>
       <div
-
-        style={{ backgroundImage: `url(${movie ? imageUrl + movie.backdrop_path : ""})` }}
-
-        className='movieBanner'>
-        <div className='movie_content'>
-          <h1 className='movie_title'>{movie ? movie.title : ""}</h1>
-          <div className='banner_buttons'>
-            <button onClick={()=>movievideo(movie.id)} className='movie_button'>Play</button>
-
+        style={{
+          backgroundImage: `url(${
+            movie ? imageUrl + movie.backdrop_path : ""
+          })`,
+        }}
+        className="movieBanner"
+      >
+        <div className="movie_content">
+          <h1 className="movie_title">{movie ? movie.title : ""}</h1>
+          <div className="banner_buttons">
+            <button
+              onClick={() => movievideo(movie.id)}
+              className="movie_button"
+            >
+              Play
+            </button>
           </div>
-          <h1 className='movie_description'>{movie ? movie.overview : ""}</h1>
-          <div className='moviedate'>
+          <h1 className="movie_description">{movie ? movie.overview : ""}</h1>
+          <div className="moviedate">
             <p>Release Date: {movie.release_date}</p>
             <p>Rating: {movie.vote_average}</p>
-
           </div>
           <div className="movie_fade"></div>
-
         </div>
-
-
       </div>
 
-      { urlid && <Youtube videoId={urlid.key} opts={opts} /> }
+      {urlid && <Youtube videoId={urlid.key} opts={opts} />}
     </div>
-
-
-
-
-
   );
 }
 
--- src/Components/navBar/navBar.jsx
+++ src/Components/navBar/navBar.jsx
@@ -1,18 +1,26 @@
-import React from 'react'
-import "./navBar.css"
+import React from "react";
+import "./navBar.css";
 function navBar() {
   return (
-    <div className='navbar'>
-      <img className='logo' src="https://upload.wikimedia.org/wikipedia/commons/thumb/0/08/Netflix_2015_logo.svg/1920px-Netflix_2015_logo.svg.png" alt="" />
-      <div className='nav'>
-        <h5 className='navitems'>Home</h5>
-        <h5 className='navitems'>Tv programmes</h5>
-        <h5 className='navitems'>Originals</h5>
-        <h5 className='navitems'>My List</h5>
-        </div>
-      <img className='avatar' src="https://i.pinimg.com/originals/0d/dc/ca/0ddccae723d85a703b798a5e682c23c1.png" alt="" />
+    <div className="navbar">
+      <img
+        className="logo"
+        src="https://upload.wikimedia.org/wikipedia/commons/thumb/0/08/Netflix_2015_logo.svg/1920px-Netflix_2015_logo.svg.png"
+        alt=""
+      />
+      <div className="nav">
+        <h5 className="navitems">Home</h5>
+        <h5 className="navitems">Tv programmes</h5>
+        <h5 className="navitems">Originals</h5>
+        <h5 className="navitems">My List</h5>
+      </div>
+      <img
+        className="avatar"
+        src="https://i.pinimg.com/originals/0d/dc/ca/0ddccae723d85a703b798a5e682c23c1.png"
+        alt=""
+      />
     </div>
-  )
+  );
 }
 
-export default navBar
+export default navBar;
